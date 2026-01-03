28e471c487ca47b0d1a9744fe3c36b4b60fa93a8
VyomsCode <vyomscode@gmail.com>
2026-01-03 22:47:30 +0530

    Added dynamic routing to filter books by category

--- src/pages/BrowseBooks.jsx
+++ src/pages/BrowseBooks.jsx
@@ -2,21 +2,34 @@
 import Navbar from '../components/Navbar'
 import BookCard from '../components/BookCard'
 import booksData from '../data/booksData'
+import { useParams } from 'react-router-dom'
 
 function BrowseBooks() {
+  const { category } = useParams()
+
+  const filteredBooks = category
+    ? booksData.filter((book) => book.category === category)
+    : booksData
+
   return (
     <>
       <Navbar />
 
       <div className="container mt-4">
-        <h1 className="text-center mb-4">Browse Books</h1>
+        <h1 className="text-center mb-4">
+          {category ? `${category.toUpperCase()} Books` : 'Browse Books'}
+        </h1>
 
         <div className="row">
-          {booksData.map((book) => (
-            <div className="col-md-4 mb-4" key={book.id}>
-              <BookCard book={book} />
-            </div>
-          ))}
+          {filteredBooks.length > 0 ? (
+            filteredBooks.map((book) => (
+              <div className="col-md-4 mb-4" key={book.id}>
+                <BookCard book={book} />
+              </div>
+            ))
+          ) : (
+            <p className="text-center">No books found in this category.</p>
+          )}
         </div>
       </div>
     </>
