ed6ed6372049f51e7d2d7f216cac831cbd3e6190
VyomsCode <vyomscode@gmail.com>
2026-01-03 22:45:58 +0530

    Implemented Browse Books page with book listing

--- src/pages/BrowseBooks.jsx
+++ src/pages/BrowseBooks.jsx
@@ -1,14 +1,24 @@
 // src/pages/BrowseBooks.jsx
 import Navbar from '../components/Navbar'
+import BookCard from '../components/BookCard'
 import booksData from '../data/booksData'
 
 function BrowseBooks() {
-  console.log(booksData) // temporary check
-
   return (
     <>
       <Navbar />
-      <h1 className="text-center mt-4">Browse Books Page</h1>
+
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Browse Books</h1>
+
+        <div className="row">
+          {booksData.map((book) => (
+            <div className="col-md-4 mb-4" key={book.id}>
+              <BookCard book={book} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
