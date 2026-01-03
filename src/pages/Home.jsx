5c7b46ea6311abeb5eedd7e3964d3fa801c51f9c
VyomsCode <vyomscode@gmail.com>
2026-01-03 22:41:35 +0530

    Implemented Home page with welcome message and book categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,39 @@
 // src/pages/Home.jsx
-import Navbar from "../components/Navbar"
+import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom'
 
 function Home() {
+  const categories = [
+    'Fiction',
+    'Non-Fiction',
+    'Sci-Fi',
+    'Mystery',
+    'Biography',
+  ]
+
   return (
     <>
-        <Navbar />
-        <h1>Home Page</h1>
+      <Navbar />
+
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Welcome to the Online Library</h1>
+
+        <h3 className="mb-3">Book Categories</h3>
+
+        <div className="list-group">
+          {categories.map((category) => (
+            <Link
+              key={category}
+              to={`/books/${category.toLowerCase()}`}
+              className="list-group-item list-group-item-action"
+            >
+              {category}
+            </Link>
+          ))}
+        </div>
+      </div>
     </>
-    )
+  )
 }
 
 export default Home
